417df177f9e9e99837e5482a6e5000e06fd97dea
Dylan Fox <dylanfox@Dylans-MacBook-Air.local>
2024-05-05 15:02:42 -0500

    changing to me

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Dylan Fox',
+    template: '%s | Dylan Fox',
   },
-  description: 'This is my portfolio.',
+  description: 'This is Dylan Fox\'s Portfolio.',
   openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
+    title: 'Dylan',
+    description: 'This is Dylan Fox\'s Portfolio.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Dylan Fox',
     locale: 'en_US',
     type: 'website',
   },
