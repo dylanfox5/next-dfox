fc4559d48b55f971adb1c6f7cffd560464bf5009
Dylan Fox <dylan.fox.kc@gmail.com>
2024-09-11 10:20:08 -0500

    updates to /projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Projects</h1>
+      <p className="mb-4">
+        {`Under construction...`}
+      </p>
+    </section>
+  )
+}
