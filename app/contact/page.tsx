cf5deb882e0e31ae3aad27476122942391eac25e
Dylan Fox <dylan.fox.kc@gmail.com>
2026-04-08 15:03:14 -0500

    change email

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -46,10 +46,10 @@ export default function Page() {
         Want to chat?
       </h2>
       <a
-        href="mailto:dfox@dylanfox.net"
+        href="mailto:dylan@dylanfox.net"
         className="text-neutral-600 dark:text-neutral-400 hover:text-neutral-900 dark:hover:text-neutral-100 transition-all underline underline-offset-2 decoration-neutral-400 dark:decoration-neutral-600 decoration-[0.1em]"
       >
-        dfox@dylanfox.net
+        dylan@dylanfox.net
       </a>
     </section>
   )
